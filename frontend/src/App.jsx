5ed4388d181ec5836f5ba4f639814202ffed44d0
Techy Nidhi <nidhi129kumari@gmail.com>
2025-07-24 09:54:42 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <p className="text-red-950">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore at
+        facere, ipsam iste libero cupiditate ullam minus excepturi sed autem
+        totam rerum impedit?
+      </p>
+    </>
+  );
+}
+
+export default App;
