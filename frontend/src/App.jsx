19043d84d966d7a50cc3393a643a924e8dbef54f
AbhiIED <abhishekreal01@gmail.com>
2025-08-02 08:42:07 +0530

    Signin page/folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
+import Signin from "./pages/Signin";
+
 function App() {
   return (
     <>
-      <p className="text-red-950">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore at
-        facere, ipsam iste libero cupiditate ullam minus excepturi sed autem
-        totam rerum impedit?
-      </p>
+  <Signin/>
+
     </>
   );
 }
